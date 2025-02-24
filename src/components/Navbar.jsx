54c21c4784028791e93047e17f31e60beab8d62c
RonakSEO <ronvkdave@gmail.com>
2025-02-24 23:50:04 +0530

    Done1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ export default function Navbar() {
       <nav className="flex items-center justify-between p-6 lg:px-8 max-w-7xl mx-auto" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="text-2xl font-bold text-blue-600">SEOsouk</span>
+            <span className="text-2xl font-bold text-blue-600">SEOSouk</span>
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -52,7 +52,7 @@ export default function Navbar() {
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="text-2xl font-bold text-blue-600">SEO Agency</span>
+              <span className="text-2xl font-bold text-blue-600">SEOSouk</span>
             </a>
             <button
               type="button"
